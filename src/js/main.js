e5b7349859eb722cb4283f9fdf154b9cf05f6d81
Aneta Grochowska <aneta.anette.g3@gmail.com>
2022-06-08 22:26:05 +0200

    improved popup button

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,10 @@
 console.log("kesjfbnkj");
 
-/* function onclickPopup() {
-    let popup = document.getElementById("button");
-    popup.classList.toggle("show");
-}
-*/ 
-
 const openPopupButtons = document.querySelectorAll('[data-modal-target]')
 const closePopupButtons = document.querySelectorAll('[data-close-button]')
 
+
+const closePopupButton = document.getElementById('x-button')
 const overlay = document.getElementById('overlay')
 
 openPopupButtons.forEach(button => {
@@ -18,12 +14,19 @@ openPopupButtons.forEach(button => {
     })
 })
 
-/* overlay.addEventListener('click', () => {
-    const popups = document.querySelectorAll('.popup.active')
+overlay.addEventListener('click', () => {
+    const popups = document.querySelectorAll('#popup.active')
     popups.forEach(popup => {
         closePopup(popup)
     })
-}) */
+}) 
+
+closePopupButton.addEventListener('click', () => {
+    const popups = document.querySelectorAll('#popup.active')
+    popups.forEach(popup => {
+        closePopup(popup)
+    })
+}) 
 
 closePopupButtons.forEach(button => {
     button.addEventListener('click', () => {
