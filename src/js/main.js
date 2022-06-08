0e9a1b1a74e66ed1e148800aea90809054255e2a
Aneta Grochowska <aneta.anette.g3@gmail.com>
2022-06-08 22:16:58 +0200

    popup js

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,41 @@ console.log("kesjfbnkj");
     popup.classList.toggle("show");
 }
 */ 
+
+const openPopupButtons = document.querySelectorAll('[data-modal-target]')
+const closePopupButtons = document.querySelectorAll('[data-close-button]')
+
+const overlay = document.getElementById('overlay')
+
+openPopupButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const popup = document.querySelector(button.dataset.modalTarget)
+        openPopup(popup)
+    })
+})
+
+/* overlay.addEventListener('click', () => {
+    const popups = document.querySelectorAll('.popup.active')
+    popups.forEach(popup => {
+        closePopup(popup)
+    })
+}) */
+
+closePopupButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const popup = button.closest('popup')
+        closePopup(popup)
+    })
+})
+
+function openPopup(popup) {
+    if (popup == null) return
+    popup.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closePopup(popup) {
+    if (popup == null) return
+    popup.classList.remove('active')
+    overlay.classList.remove('active')
+}
